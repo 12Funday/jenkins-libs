e70b946af31a31678667d48156b015f8901ea897
12funday <adispandey12@gmail.com>
2026-03-15 18:52:41 +0700

    add condition

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -1,10 +1,26 @@
 def call(String repoUrl, String imageName) {
-    // Checkout repo project
+    // Checkout project repo
     git branch: 'main', url: repoUrl
 
-    // Build Docker image
-    sh "docker build -t ${imageName} ."
+    // Ambil commit ID terakhir
+    def commitId = sh(script: "git rev-parse --short HEAD", returnStdout: true).trim()
 
-    // Run tests
+    // Cek kalau ada Git tag yang cocok dengan commit terakhir
+    def gitTag = sh(script: "git describe --tags --exact-match || echo ''", returnStdout: true).trim()
+
+    // Tentukan tag image
+    def imageTag = gitTag ?: commitId
+    echo "✅ Tag Docker image: ${imageTag}"
+
+   // Retry build Docker sampai 3 kali jika gagal
+    retry(3) {
+        echo "🔄 build Docker..."
+        sh "docker build -t ${imageName}:${imageTag} ."
+    }
+
+    // Optional: bisa juga build 'latest' tag
+    sh "docker tag ${imageName}:${imageTag} ${imageName}:latest"
+
+    // Run tests (contoh)
     sh "echo 'Running tests...'"
 }
